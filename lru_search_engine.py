cf53d6240a396fc7f4d56880144a5f1d2a634425
zhengquantao <zhegnquantao80@gmail.com>
2021-11-20 19:46:36 +0800

    init

---
+++ lru_search_engine.py
@@ -0,0 +1,38 @@
+import pylru
+
+from SearchEngine.bow_inverted_index_engine import BOWInvertedIndexEngine
+from SearchEngine.engine_base import main
+
+
+class LRUCache(object):
+    def __init__(self, size=32):
+        self.cache = pylru.lrucache(size)
+
+    def has(self, key):
+        return key in self.cache
+
+    def get(self, key):
+        return self.cache[key]
+
+    def set(self, key, value):
+        self.cache[key] = value
+
+
+class BOWInvertedIndexEngineWithCache(BOWInvertedIndexEngine, LRUCache):
+    def __init__(self):
+        super(BOWInvertedIndexEngineWithCache, self).__init__()
+        LRUCache.__init__(self)
+
+    def search(self, query):
+        if self.has(query):
+            print('cache hit!')
+            return self.get(query)
+
+        result = super(BOWInvertedIndexEngineWithCache, self).search(query)
+        self.set(query, result)
+
+        return result
+
+
+search_engine = BOWInvertedIndexEngineWithCache()
+main(search_engine)
